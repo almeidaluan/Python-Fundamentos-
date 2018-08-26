ab6a3653e6888dfb4cc0880be6930c23999afc37
Luan <almeida.luan099@gmail.com>
2018-08-26 17:17:24 -0300

    First commit CAP02 - Trabalhando com numeros

---
+++ cursoDSA/CAP02/01-Numeros.py
@@ -0,0 +1,122 @@
+##############################
+#                            #
+#   Operações matematicas    #
+#                            #
+##############################
+
+#Soma
+
+print( 4 + 4 )
+
+#Substração
+
+print( 4 - 3 )
+
+#Multiplicação
+
+print( 3 * 3 )
+
+#Divisão
+
+print( 3 / 2 )
+
+# Potência
+
+print ( 4 ** 2 )
+
+# Módulo(Resto)
+
+print( 10 % 3 )
+
+##############################
+#                            #
+#       Função Type          #
+#                            #
+##############################
+
+# Type com inteiro
+
+print(type(5))
+
+
+# Type com float
+
+print(type(5.0))
+
+# Type com String
+
+a = 'Eu sou uma String'
+print(type(a))
+
+##############################
+#                            #
+# Operação com números float #
+#                            #
+##############################
+
+print( 3.1 + 6.4 )
+
+print( 4 + 4.0 )
+
+print( 4 + 4 )
+
+#Resultado é um numero float
+print( 4 / 2 )
+
+#Resultado é um numero inteiro
+print( 4 // 2 )
+
+print( 4 / 3.0 )
+
+print ( 4 // 3.0 )
+
+##############################
+#                            #
+#       Conversão            #
+#                            #
+##############################
+
+
+float ( 9 )
+
+int ( 6.0 )
+
+int ( 6.5 )
+
+
+##############################
+#                            #
+#       Hexadecimal          #
+#                            #
+##############################
+
+hex ( 394 )
+
+hex ( 217 )
+
+bin ( 286 )
+
+bin ( 390 )
+
+##############################
+#                            #
+#   Funções ABS,ROUND,POW    #
+#                            #
+##############################
+
+#Retorna o valor absoluto
+abs ( -8 )
+
+#Retorna o valor absoluto
+abs( 8 )
+
+#Retorna o valor com arredondamento
+round( 3.14151922,2 )
+
+#Potência
+pow(4,2)
+
+#Potência
+pow(5,3)
+
+
